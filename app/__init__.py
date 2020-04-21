eb407cb23a6b012fd260b464398bdf38f24a3cba
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-04-21 19:31:08 +0300

    Init project setup

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app.routes import *
+from app.models import *
