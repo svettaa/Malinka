6306117bc8dc2592bc5e66de9766669e9953f22f
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-05-05 23:32:05 +0300

    Removed date functions and several asserts

--- app/api/asserts/asserts_appointment.py
+++ app/api/asserts/asserts_appointment.py
@@ -4,17 +4,18 @@ from app import db
 from app.models import Appointment
 
 
-def is_future_appointment(appointment: Appointment):
-    return (appointment.appoint_date.date() > datetime.now().date() or
-            (appointment.appoint_date.date() == datetime.now().date() and
-             appointment.start_time.time() > datetime.now().time()))
+# def is_future_appointment(appointment: Appointment):
+#     return (appointment.appoint_date.date() > datetime.now().date() or
+#             (appointment.appoint_date.date() == datetime.now().date() and
+#              appointment.start_time.time() > datetime.now().time()))
 
 
 def assert_appointment_is_hired(appointment: Appointment):
-    if is_future_appointment(appointment) and \
-            not db.session.execute(""" SELECT is_hired
-                                       FROM Master
-                                       WHERE id = :master_id;
-                                   """,
-                                   {'master_id': appointment.master_id}).scalar():
-        raise AssertionError('Даний майстер звільнений')
+    pass
+    # if is_future_appointment(appointment) and \
+    #         not db.session.execute(""" SELECT is_hired
+    #                                    FROM Master
+    #                                    WHERE id = :master_id;
+    #                                """,
+    #                                {'master_id': appointment.master_id}).scalar():
+    #     raise AssertionError('Даний майстер звільнений')
