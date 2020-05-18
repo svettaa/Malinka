aa18950ef9264147f57a93c66e5e6b4a8c11eb0e
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-05-18 13:54:40 +0300

    env variables

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,27 @@ from flask_admin import Admin
 from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
 from flask_socketio import SocketIO
+from datetime import time
+
+import os
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+app.config['DEBUG'] = os.environ.get('DEBUG')
+app.config['CSRF_ENABLED'] = os.environ.get('CSRF_ENABLED')
+app.config['CSRF_SESSION_KEY'] = os.environ.get('CSRF_SESSION_KEY')
+app.config['WTF_CSRF_SECRET_KEY'] = os.environ.get('WTF_CSRF_SECRET_KEY')
+app.config['WTF_CSRF_TIME_LIMIT'] = int(os.environ.get('WTF_CSRF_TIME_LIMIT'))
+
+app.config['WORKING_DAY_START'] = time(9, 0, 0)
+app.config['WORKING_DAY_END'] = time(20, 0, 0)
 
 login_manager = LoginManager(app)
 db = SQLAlchemy(app)
