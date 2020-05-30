c52364891e589df726122fc0efbad88436350243
Sveta <svitlana.lukichova@gmail.com>
2020-05-30 19:20:40 +0300

    Paint 2.0

--- app/static/js/api/admin/paints.js
+++ app/static/js/api/admin/paints.js
@@ -7,3 +7,39 @@ function sendRequestGetAllPaints(callback) {
         success: callback
     });
 }
+
+function sendRequestDeletePaint(id, callback) {
+    $.ajax({
+        type: "DELETE",
+        url: "/api/paints/" + id,
+        success: callback
+    });
+}
+
+function sendRequestEditPaint(id, data, callback) {
+    $.ajax({
+        type: "PUT",
+        url: "/api/paints/" + id,
+        data: data,
+        success: callback
+    });
+}
+
+
+function sendRequestGetPaint(id, callback) {
+    $.ajax({
+        type: "GET",
+        url: "/api/paints/" + id,
+        success: callback
+    });
+}
+
+function sendRequestAddPaint(data, callback) {
+    $.ajax({
+        type: "POST",
+        url: "/api/paints",
+        data: data,
+        success: callback
+    });
+}
+
