076730b46e9f65f0e6a3a271dee17f9ffa2d83da
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-06-10 23:30:12 +0300

    Keep refactoring

--- app/routes/master_routes.py
+++ app/routes/master_routes.py
@@ -5,8 +5,8 @@ from app import app
 from app.login import master_only
 
 
-@app.route('/cabinet/timetable', methods=['GET'])
+@app.route('/profile/timetable', methods=['GET'])
 @login_required
 @master_only
 def master_timetable_get():
-    return render_template('master_timetable.html')
+    return render_template('master/timetable.html')
